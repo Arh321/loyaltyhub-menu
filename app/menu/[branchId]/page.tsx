cd6e8853f736b32eeac4e8f05b05bb27bb696821
hasanzadeh <alirezawtf321@gmail.com>
2025-03-15 12:07:17 +0330

    regenerating project

--- app/menu/[branchId]/page.tsx
+++ app/menu/[branchId]/page.tsx
@@ -1,5 +1,4 @@
 import MenuCategories from "@/components/menu-page/menu-categories";
-import { useBranchInfo } from "@/app/hooks/useBranches";
 export default function MenuPage() {
   return (
     <div className="h-screen overflow-auto   text-center pb-40">
