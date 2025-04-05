f30ecd51fdebe877bd73d3c315afe76635008a3f
hasanzadeh <alirezawtf321@gmail.com>
2025-04-05 09:39:08 +0330

    fixing loading in products page

---
+++ app/departments/[depId]/products/loading.tsx
@@ -0,0 +1,11 @@
+import SplashScreen from "@/components/loading/splash-screen";
+
+const LoadingProducts = () => {
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <SplashScreen />
+    </div>
+  );
+};
+
+export default LoadingProducts;
