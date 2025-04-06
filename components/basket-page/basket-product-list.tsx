d5fee75ca544b3b034d3b805d3a2b9979e0d2ba1
hasanzadeh <alirezawtf321@gmail.com>
2025-04-07 00:58:26 +0330

    fixing basket product list

--- components/basket-page/basket-product-list.tsx
+++ components/basket-page/basket-product-list.tsx
@@ -2,19 +2,25 @@ import { IBasketState } from "@/types/menu/menu-types";
 import ImageWithLoader from "../image-with-loader/image-with-loader";
 import { departmentsImageData } from "../departments-page/departments-image-data";
 import { useMemo } from "react";
-import ProductCardButton from "../products-page/products-section/product-card-button";
-
+import CTAButton from "../shared-components/cta-button/cta-button";
+import { Icon } from "@iconify/react/dist/iconify.js";
+import {
+  decrementQuantity,
+  incrementQuantity,
+} from "@/redux/basket-slice/basketSlice";
+import { useDispatch } from "react-redux";
 interface BasketProductListProps {
   product: IBasketState;
 }
 
 const BasketProductList = ({ product }: BasketProductListProps) => {
+  const dispatch = useDispatch();
   const image = useMemo(() => {
     return (
-      departmentsImageData.find((image) => image.id === product.id)?.image
-        .src ?? ""
+      departmentsImageData.find((image) => image.id === product.productId)
+        ?.image.src ?? ""
     );
-  }, [product.id]);
+  }, [product.productId]);
 
   return (
     <div className="w-full flex items-center justify-between px-4">
@@ -39,7 +45,25 @@ const BasketProductList = ({ product }: BasketProductListProps) => {
         </div>
       </div>
       <div className="w-[100px] ">
-        <ProductCardButton product={product} />
+        <div className="w-full justify-between flex items-center gap-2">
+          <CTAButton
+            className="text-sm p-[2px] w-max h-max border border-light-primary !text-light-primary !bg-transparent"
+            onClick={() =>
+              dispatch(incrementQuantity({ id: product.productId }))
+            }
+          >
+            <Icon icon="basil:plus-outline" width="24" height="24" />
+          </CTAButton>
+          <span>{product.quantity}</span>
+          <CTAButton
+            className="text-sm p-[2px] w-max h-max border border-light-primaryText text-light-primaryText !bg-transparent"
+            onClick={() =>
+              dispatch(decrementQuantity({ id: product.productId }))
+            }
+          >
+            <Icon icon="jam:minus" width="24" height="24" />
+          </CTAButton>
+        </div>
       </div>
     </div>
   );
